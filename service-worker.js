a10f2231645673ac4b67a3a2854a79e2c43c5463
Franco Allovero <fra98nba@gmail.com>
2024-08-21 20:51:06 -0300

    Primer commiteada

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = 'tierrasur-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/static/styles.css',
+    '/script.js',
+    '/manifest.json',
+    'static/images/tierrasur.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
---
+++ static/app.js
@@ -0,0 +1,12 @@
+// Aquí puedes agregar la lógica de JavaScript para manejar el formulario de inicio de sesión.
+
+document.querySelector('.login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Lógica de autenticación
+    alert('Iniciar sesión');
+});
+
+document.querySelector('.btn-register').addEventListener('click', function() {
+    // Lógica para manejar el registro
+    alert('Registro');
+});
