f55e0ba05ed461e04b7792d6373866fcca8e3864
Franco Allovero <fra98nba@gmail.com>
2024-08-26 20:51:00 -0300

    Actualizacion .js 2.0

--- tierrasur/service-worker.js
+++ tierrasur/service-worker.js
@@ -1,14 +1,11 @@
 const cacheName = 'tierrasur-v1';
 const filesToCache = [
     '/',
-    '/templates/base.html',
-    '/templates/login.html',
-    '/templates/register.html',
     '/static/style.css',
     '/static/style_home.css',
     '/script.js',
     '/manifest.json',
-    'static/images/tierrasur.png'
+    '/static/images/tierrasur.png'
 ];
 
 self.addEventListener('install', event => {
