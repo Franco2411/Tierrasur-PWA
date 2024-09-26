7e0d6af4b0f05d256be9bb99ae72e9c95e4f50a2
Franco Allovero <fra98nba@gmail.com>
2024-09-26 20:17:22 -0300

    Creacion api obtencion de registros y acomo de ux en tablets

--- tierrasur/home.py
+++ tierrasur/home.py
@@ -81,3 +81,65 @@ def combo_insumo_labor():
     registros = c.fetchall()
 
     return jsonify(registros)
+
+# Api para recuperar los registros hechos por un usuario
+@bp.route('/api/get_registers', methods=['GET'])
+def get_registers():
+    db, c = get_db()
+    data = request.get_json()
+    error = None
+    registros = []
+
+    #id_usuario = g.user['nick']
+    id_usuario = data.get('nick')
+    fecha_inicio = datetime.strptime(data.get('fecha_inicio'), '%d/%m/%Y')
+    fecha_final = datetime.strptime(data.get('fecha_final'), '%d/%m/%Y')
+    logging.debug(f'Los datos enviados via api son id: {id_usuario}, fecha1: {fecha_inicio}, fecha2: {fecha_final}')
+
+    if not id_usuario or not fecha_inicio or not fecha_final:
+        error = 'Se tienen que completar todos los datos'
+        return jsonify({'success': False, 'message': error})
+    else:
+        c.execute(
+        'select * from ordenes where creado_por = %s and fecha between %s and %s', (id_usuario, fecha_inicio, fecha_final)
+        )
+        ordenes = c.fetchall()
+        logging.debug(ordenes)
+
+        if not ordenes:
+            error = 'No existen registros para el rango de fechas especificado.'
+            logging.debug('Entre al if donde no hay ordenes')
+            return jsonify({'success': True, 'message': error})
+        else:
+            for orden in ordenes:
+                nro_c = orden['id']
+                logging.debug(f'El nro_c es: {nro_c}')
+                c.execute(
+                    'select * from hoja_tareas where nro_c = %s order by fecha desc', (nro_c,)
+                )
+                reg = c.fetchall()
+                for r in reg:
+                    reg_dic = {
+                        'id': r['id'],
+                        'up': r['up'],
+                        'lote': r['lote'],
+                        'actividad': r['actividad'],
+                        'fecha': r['fecha'].isoformat(timespec='seconds'),
+                        'cantidad': r['cant'],
+                        'detalle': r['detalle'],
+                        'codigo': r['codigo'],
+                        'uta': r['uta'],
+                        'restar': r['restar'],
+                        'campania': r['campa'],
+                        'pc': r['pc'],
+                        'fechata': r['fechata'],
+                        'nro_c': r['nro_c'],
+                        'cplan': r['cplan'],
+                        'borrador': r['borrador'],
+                        'precio': r['precio']
+                    }
+                    registros.append(reg_dic)
+            return jsonify({'success': True, 'data': registros})
+
+    
+
