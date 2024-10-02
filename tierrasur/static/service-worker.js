4934967102ab7461431c51d26dca014a5831e9a1
Franco Allovero <fra98nba@gmail.com>
2024-10-01 22:09:46 -0300

    Agregado de serviceworker y logo de la app

--- tierrasur/static/service-worker.js
+++ tierrasur/static/service-worker.js
@@ -1,24 +1,51 @@
-const cacheName = 'tierrasur-v1';
-const filesToCache = [
-    '/',
-    '/static/style.css',
-    '/static/images/tierrasur.png'
+const CACHE_NAME = "v1";
+const urlsToCache = [
+    "/",
+    "/static/style.css",
+    "/static/style_home.css",
+    "/static/scripts.js",
+    "/static/scripts_auth.js",
+    "/static/images/fondo.jpg",
+    "/static/images/ic_launcher.png",
+    "/static/images/tierrasur.png"
 ];
 
-self.addEventListener('install', event => {
+// Instalación del Service Worker
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            return cache.addAll(filesToCache);
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log("Archivos en caché");
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activación del Service Worker
+self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+// Intercepta las peticiones de la aplicación
+self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
