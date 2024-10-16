ff773988723caae61e78db64c8fec411dfed8271
Franco Allovero <fra98nba@gmail.com>
2024-10-16 19:46:02 -0300

    Subida de CORS

--- tierrasur/__init__.py
+++ tierrasur/__init__.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app.config.from_mapping(
         SECRET_KEY='mikey',
